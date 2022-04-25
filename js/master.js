24bfc98d008ef5a12e8459e3dd51d7d8ad5fcebe
Passant Abdelgalil <basant.a.abdelgalil@gmail.com>
2022-04-25 21:17:52 +0200

    added about-us, our skills, gallery, and timeline sections

--- js/master.js
+++ js/master.js
@@ -15,3 +15,93 @@ settingsIcon.onclick = (e) => {
   // toggle openning of settings container
   settingsIcon.parentElement.classList.toggle("open");
 };
+
+// skills selector
+let ourSkills = document.querySelector(".skills");
+window.onscroll = () => {
+  // skills offset top
+  let skillsOffsetTop = ourSkills.offsetTop;
+  // skills outer height
+  let skillsOuterHeight = ourSkills.offsetHeight;
+  // window height
+  let windowHeight = window.innerHeight;
+  // window scroll top
+  let windowScrollTop = window.scrollY;
+  console.log(windowScrollTop);
+  let allSkills = document.querySelectorAll(
+    ".skill-box .skill-box__progress span"
+  );
+  if (windowScrollTop > skillsOffsetTop + skillsOuterHeight - windowHeight) {
+    allSkills.forEach((skill) => {
+      skill.style.width = skill.dataset.progress;
+    });
+  } else {
+    allSkills.forEach((skill) => (skill.style.width = "0px"));
+  }
+};
+
+// create image popup
+let ourGallery = document.querySelectorAll(".gallery img");
+
+ourGallery.forEach((img) => {
+  img.addEventListener("click", (e) => {
+    // Create overlay element
+    let overlay = document.createElement("div");
+
+    // add class to overlay
+    overlay.className = "popup-overlay";
+
+    // append overlay to the body
+    document.body.appendChild(overlay);
+
+    // create popup
+    let popupBox = document.createElement("div");
+
+    // Add class to popupBox
+    popupBox.className = "popup-box";
+
+    if (img.alt !== null) {
+      // create heading
+      let imgHeading = document.createElement("h3");
+      // create text for heading
+      let imgText = document.createTextNode(img.alt);
+      // append the text to the heading
+      imgHeading.appendChild(imgText);
+
+      // append heading to popupBox
+      popupBox.appendChild(imgHeading);
+    }
+
+    // create the img
+    let popupImage = document.createElement("img");
+
+    // set image src
+    popupImage.src = img.src;
+
+    // add image to popupBox
+    popupBox.appendChild(popupImage);
+
+    // append popupBox to body
+    document.body.appendChild(popupBox);
+
+    // Create the close span
+    let closeButton = document.createElement("span");
+    // Create the close button text
+    let closeButtonText = document.createTextNode("X");
+    // append text to close button
+    closeButton.appendChild(closeButtonText);
+    // add class to close button
+    closeButton.className = "close-button";
+    // add close button to popupBox
+    popupBox.appendChild(closeButton);
+  });
+});
+// close popup
+document.addEventListener("click", (e) => {
+  console.log(e.target.parentNode);
+  // remove the current popup
+  if (e.target.className == "close-button") {
+    e.target.parentNode.remove();
+    document.querySelector(".popup-overlay").remove();
+  }
+});
--- js/settings/background.js
+++ js/settings/background.js
@@ -1,5 +1,5 @@
 export default class BackgroundManager {
-  randomBackground;
+  #randomBackground;
   backgroundImgs = [
     "img01.jpg",
     "img02.jpg",
@@ -7,7 +7,7 @@ export default class BackgroundManager {
     "img04.jpg",
     "img05.jpg",
   ];
-  backgroundIntervalObj;
+  #backgroundIntervalObj;
   landingPage = document.querySelector(".landing-page");
   constructor() {
     this.setup();
@@ -16,30 +16,29 @@ export default class BackgroundManager {
     let randomBackgroundOptions = document.querySelectorAll(".options-list li");
 
     // load background setting option from local storage
-    this.randomBackground = window.localStorage.getItem("random-background");
-
-    // unmark previously marked active option
-    randomBackgroundOptions.forEach((option) => {
-      if (option.dataset.random_background === this.randomBackground)
-        option.classList.add("active");
-      else option.classList.remove("active");
-      option.addEventListener("click", (e) => this.updateBackgroundOption(e));
-    });
+    this.#randomBackground = window.localStorage.getItem("random-background");
 
     // if not previously stored, set a default value = true
-    if (this.randomBackground == null) {
-      this.randomBackground = "true";
+    if (this.#randomBackground === null) {
+      this.#randomBackground = "true";
 
       // load background setting option from local storage
-      window.localStorage.setItem("random-background", true);
+      window.localStorage.setItem("random-background", "true");
     }
-    if (this.randomBackground === "true") {
+    if (this.#randomBackground === "true") {
       this.pickRandomImage();
     } else {
       document
         .querySelector('.options-list li[data-random_background="false"]')
         .classList.add("active");
     }
+    // unmark previously marked active option
+    randomBackgroundOptions.forEach((option) => {
+      if (option.dataset.random_background === this.#randomBackground)
+        option.classList.add("active");
+      else option.classList.remove("active");
+      option.addEventListener("click", (e) => this.updateBackgroundOption(e));
+    });
   }
 
   updateBackground() {
@@ -54,17 +53,17 @@ export default class BackgroundManager {
       .forEach((elem) => elem.classList.remove("active"));
     // mark current element as active
     e.target.classList.add("active");
-    this.randomBackground = e.target.dataset.random_background;
-    window.localStorage.setItem("random-background", this.randomBackground);
+    this.#randomBackground = e.target.dataset.random_background;
+    window.localStorage.setItem("random-background", this.#randomBackground);
     // set / clear randomizing background based on selected option
-    if (e.target.dataset.random_background === "true") {
+    if (this.#randomBackground === "true") {
       this.pickRandomImage();
     } else {
-      clearInterval(this.backgroundIntervalObj);
+      clearInterval(this.#backgroundIntervalObj);
     }
   }
   pickRandomImage() {
-    this.backgroundIntervalObj = setInterval(
+    this.#backgroundIntervalObj = setInterval(
       () => this.updateBackground(),
       10000
     );
